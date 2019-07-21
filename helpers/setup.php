a6eaf4f0f5caf66b58be19e51b719422717ea855
saibhaskardevatha <devatha@iitg.ac.in>
2019-07-21 20:15:59 +0530

    4th year

--- helpers/setup.php
+++ helpers/setup.php
@@ -9,19 +9,34 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $_SESSION['branch']=$branch;
 
     include('config.php');
-
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    
     switch($year){
         case '8':
-            header('Location: ../edit/3.php');
+            header('Location: ../edit/3');
             break;
         case '7':
-            header('Location: ../edit/5.php');
+            header('Location: ../edit/5');
             break;
         case '6':
-            header('Location: ../edit/7.php');
+            $sql = "SELECT * from roll_data_6 where roll = $roll";
+            $result = mysqli_query($conn, $sql);
+            $result = mysqli_fetch_assoc($result);
+
+            if(empty($result)){
+                header('Location: ../edit/7'); 
+            }else{
+                header('Location: ../tt/4');
+            }
+            mysqli_close($conn);
             break;
         case '9':
-            header('Location: ../edit/8.php');
+            header('Location: ../tt/1');
             break;
     }
 }else{
