4020e35e2a258ec805349f4f103dcc35ff24b538
saibhaskardevatha <devatha@iitg.ac.in>
2019-07-21 12:32:50 +0530

    Config Edits

--- helpers/config.php
+++ helpers/config.php
@@ -1,6 +1,6 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "cepstrum-timetable";
+    $servername = "den1.mysql2.gear.host";
+    $username = "cepstrum";
+    $password = "!cepstrum";
+    $dbname = "cepstrum";
 ?>
