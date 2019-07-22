ef16f0b5c07f8e25745de576a6a83f2400e09b45
saibhaskardevatha <devatha@iitg.ac.in>
2019-07-22 12:07:03 +0530

    All admin setup

---
+++ admin/helpers/junior.php
@@ -0,0 +1,57 @@
+<?php
+    $type = $_POST["type"];
+    $id = $_POST["id"];
+    $name = $_POST["name"];
+    $venue = $_POST["venue"];
+    $day1 = $_POST["day1"];
+    $day2 = $_POST["day2"];
+    $day3 = $_POST["day3"];
+    $time1_old = $_POST["time1"];
+    $time1_explode = explode('|', $time1_old);
+    $time2_old = $_POST["time2"];
+    $time2_explode = explode('|', $time2_old);
+    $time3_old = $_POST["time3"];
+    $time3_explode = explode('|', $time3_old);
+    $time1 = $time1_explode[0];
+    $time2 = $time2_explode[0];
+    $time3 = $time3_explode[0];
+    $time4 = $time1_explode[1];
+    $time5 = $time2_explode[1];
+    $time6 = $time3_explode[1];
+
+    if($type == 'dep'){
+        $event_id = 'event-1';
+    }else if($type == 'hss'){
+        $event_id = 'event-2';
+    }
+
+    include("../../helpers/config.php");
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $sql1 = "INSERT INTO courses_junior (type, id, name)
+    VALUES ('$type', '$id', '$name')";
+
+    $sql2 = "INSERT INTO {$day1} (id, name, start_time, end_time, venue, event_id)
+    VALUES ('$id', '$name', '$time1', '$time4', '$venue', '$event_id')";
+
+    $sql3 = "INSERT INTO {$day2} (id, name, start_time, end_time, venue, event_id)
+    VALUES ('$id', '$name', '$time2', '$time5', '$venue', '$event_id')";
+
+    $sql4 = "INSERT INTO {$day3} (id, name, start_time, end_time, venue, event_id)
+    VALUES ('$id', '$name', '$time3', '$time6', '$venue', '$event_id')";
+
+    if ($conn->query($sql1) === TRUE && $conn->query($sql2) === TRUE && $conn->query($sql3) === TRUE && $conn->query($sql4) === TRUE) {
+        echo '<script type="text/javascript">
+            window.location = "../junior.php"
+        </script>';
+    } else {
+        echo "Error: " . $sql1 . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+?>
--- admin/senior.php
+++ admin/senior.php
@@ -1,9 +1,9 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Timetable</title>
+		<title>Admin Senior</title>
 		<meta charset="utf-8" />
-        <link rel="shortcut icon" href="assets/favicon.png" type="image/png"/>
+        <link rel="shortcut icon" href="../assets/favicon.png" type="image/png"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="../assets/css/main.css" />
 	</head>
